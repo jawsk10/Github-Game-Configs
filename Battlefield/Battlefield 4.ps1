1eed116e1959ec5c518b5aa5403fdb974931e3e7
FR33THY <fr33thy@gmail.com>
2025-03-29 16:53:47 +1100

    Update Battlefield 4.ps1

--- Battlefield/Battlefield 4.ps1
+++ Battlefield/Battlefield 4.ps1
@@ -94,5 +94,27 @@
     return $OpenFileDialog.FileName
     }
 
-Write-Host "In Progress"
+# create config folder
+New-Item -Path "$env:USERPROFILE\Documents\Battlefield 4" -Name "settings" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+New-Item -Path "$env:USERPROFILE\OneDrive\Documents\Battlefield 4" -Name "settings" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+Clear-Host
+# download and replace config files           
+Get-FileFromWeb -URL "https://github.com/FR33THYFR33THY/Github-Game-Configs/raw/refs/heads/main/Battlefield/Battlefield%204/PROFSAVE" -File "$env:TEMP\PROFSAVE"
+Copy-Item -Path "$env:TEMP\PROFSAVE" -Destination "$env:USERPROFILE\Documents\Battlefield 4\settings\PROFSAVE" -Force -ErrorAction SilentlyContinue | Out-Null
+Copy-Item -Path "$env:TEMP\PROFSAVE" -Destination "$env:USERPROFILE\OneDrive\Documents\Battlefield 4\settings\PROFSAVE" -Force -ErrorAction SilentlyContinue | Out-Null
+Remove-Item -Path "$env:TEMP\PROFSAVE" -Force -ErrorAction SilentlyContinue | Out-Null
+Clear-Host
+Get-FileFromWeb -URL "https://github.com/FR33THYFR33THY/Github-Game-Configs/raw/refs/heads/main/Battlefield/Battlefield%204/PROFSAVE_profile" -File "$env:TEMP\PROFSAVE_profile"
+Copy-Item -Path "$env:TEMP\PROFSAVE_profile" -Destination "$env:USERPROFILE\Documents\Battlefield 4\settings\PROFSAVE_profile" -Force -ErrorAction SilentlyContinue | Out-Null
+Copy-Item -Path "$env:TEMP\PROFSAVE_profile" -Destination "$env:USERPROFILE\OneDrive\Documents\Battlefield 4\settings\PROFSAVE_profile" -Force -ErrorAction SilentlyContinue | Out-Null
+Remove-Item -Path "$env:TEMP\PROFSAVE_profile" -Force -ErrorAction SilentlyContinue | Out-Null
+Clear-Host
+# pick install folder
+Write-Host "Select Battlefield 4 install folder:"
+$ConfigFolder1 = Show-ModernFilePicker -Mode Folder
+Clear-Host
+# download and replace config files
+Get-FileFromWeb -URL "https://github.com/FR33THYFR33THY/Github-Game-Configs/raw/refs/heads/main/Battlefield/Battlefield%204/user.cfg" -File "$ConfigFolder1\user.cfg"
+Clear-Host
+Write-Host "Battlefield 4 config applied . . ."
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
